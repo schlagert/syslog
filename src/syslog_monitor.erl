ccd08a070831b5970ca2978de14611d8b21d7761
Tobias Schlager <tobias.schlager@lindenbaum.eu>
2013-04-15 08:59:08 +0200

    Add copyright notices.

--- src/syslog_monitor.erl
+++ src/syslog_monitor.erl
@@ -1,4 +1,6 @@
 %%%=============================================================================
+%%% Copyright 2013, Tobias Schlager <schlagert@github.com>
+%%%
 %%% Permission to use, copy, modify, and/or distribute this software for any
 %%% purpose with or without fee is hereby granted, provided that the above
 %%% copyright notice and this permission notice appear in all copies.
