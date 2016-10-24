ac7053dc517c065875ece2eae04f5bbb9fcf3a2f
Rich Cavanaugh <rcavanaugh@gmail.com>
2016-10-24 09:08:49 -0400

    move get_date/1 and truncate/2 to the syslog_lib module

--- src/syslog_lib.erl
+++ src/syslog_lib.erl
@@ -27,7 +27,9 @@
          get_property/2,
          get_pid/1,
          get_utc_datetime/1,
-         get_utc_offset/2]).
+         get_utc_offset/2,
+         truncate/2,
+         get_date/1]).
 
 -define(GET_ENV(Property), application:get_env(syslog, Property)).
 
@@ -142,6 +144,38 @@ get_utc_offset(Utc, Local) ->
     {0, {H, Mi, 0}} = time_difference(Local, Utc),
     {$-, H, Mi}.
 
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% Returns a truncated string.
+%% @end
+%%------------------------------------------------------------------------------
+-spec truncate(pos_integer(), string() | binary()) -> string() | binary().
+
+truncate(L, S) when length(S) =< L -> S;
+truncate(L, S) when size(S) =< L   -> S;
+truncate(L, S) when is_list(S)     -> string:substr(S, 1, L);
+truncate(L, S) when is_binary(S)   -> binary:part(S, 0, L).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% Returns a truncated string.
+%% @end
+%%------------------------------------------------------------------------------
+-spec get_date(syslog:datetime()) -> list().
+
+get_date({UtcDatetime, MicroSecs}) ->
+    LocaDatetime = erlang:universaltime_to_localtime(UtcDatetime),
+    get_date(LocaDatetime, UtcDatetime, MicroSecs).
+get_date(Utc = {{Y, Mo, D}, {H, Mi, S}}, Utc, Micro) ->
+    [integer_to_list(Y), $-, digit(Mo), $-, digit(D), $T,
+     digit(H), $:, digit(Mi), $:, digit(S), $., micro(Micro), $Z];
+get_date(Local = {{Y, Mo, D}, {H, Mi, S}}, Utc, Micro) ->
+    {Sign, OH, OMi} = syslog_lib:get_utc_offset(Utc, Local),
+    [integer_to_list(Y), $-, digit(Mo), $-, digit(D), $T,
+     digit(H), $:, digit(Mi), $:, digit(S), $., micro(Micro),
+     Sign, digit(OH), $:, digit(OMi)].
+
 %%%=============================================================================
 %%% internal functions
 %%%=============================================================================
@@ -206,6 +240,31 @@ time_difference(T1, T2) ->
       calendar:datetime_to_gregorian_seconds(T2) -
           calendar:datetime_to_gregorian_seconds(T1)).
 
+%%------------------------------------------------------------------------------
+%% @private
+%%------------------------------------------------------------------------------
+digit(0)  -> "00";
+digit(1)  -> "01";
+digit(2)  -> "02";
+digit(3)  -> "03";
+digit(4)  -> "04";
+digit(5)  -> "05";
+digit(6)  -> "06";
+digit(7)  -> "07";
+digit(8)  -> "08";
+digit(9)  -> "09";
+digit(N)  -> integer_to_list(N).
+
+%%------------------------------------------------------------------------------
+%% @private
+%%------------------------------------------------------------------------------
+micro(M) when M < 10     -> ["00000", integer_to_list(M)];
+micro(M) when M < 100    -> ["0000", integer_to_list(M)];
+micro(M) when M < 1000   -> ["000", integer_to_list(M)];
+micro(M) when M < 10000  -> ["00", integer_to_list(M)];
+micro(M) when M < 100000 -> ["0", integer_to_list(M)];
+micro(M)                 -> integer_to_list(M).
+
 %%%=============================================================================
 %%% TESTS
 %%%=============================================================================
@@ -237,5 +296,26 @@ get_pid_test() ->
     ?assertEqual(<<"init">>, get_pid(init)),
     ?assertEqual(<<"init">>, get_pid(whereis(init))),
     ?assertEqual(list_to_binary(pid_to_list(self())), get_pid(self())).
+
+truncate_test() ->
+    ?assertEqual("",        truncate(0, "")),
+    ?assertEqual("",        truncate(1, "")),
+    ?assertEqual("",        truncate(0, "123")),
+    ?assertEqual("1",       truncate(1, "123")),
+    ?assertEqual("12",      truncate(2, "123")),
+    ?assertEqual("123",     truncate(3, "123")),
+    ?assertEqual("123",     truncate(4, "123")),
+    ?assertEqual(<<>>,      truncate(0, <<>>)),
+    ?assertEqual(<<>>,      truncate(1, <<>>)),
+    ?assertEqual(<<>>,      truncate(0, <<"123">>)),
+    ?assertEqual(<<"1">>,   truncate(1, <<"123">>)),
+    ?assertEqual(<<"12">>,  truncate(2, <<"123">>)),
+    ?assertEqual(<<"123">>, truncate(3, <<"123">>)),
+    ?assertEqual(<<"123">>, truncate(4, <<"123">>)).
+
+get_date_test() ->
+    Datetime = {{{2013,4,6},{21,20,56}},908235},
+    Rx = "2013-04-06T\\d\\d:\\d\\d:56\\.908235(Z|(\\+|-)\\d\\d:\\d\\d)",
+    ?assertMatch({match, _}, re:run(lists:flatten(get_date(Datetime)), Rx)).
 
 -endif. %% TEST
