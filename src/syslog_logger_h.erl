afa22abe282ead1b6421b27a9d6469a24b776e66
Tobias Schlager <tobias.schlager@lindenbaum.eu>
2021-01-21 15:32:37 +0100

    Remove unnecessary dialyzer warning suppression

--- src/syslog_logger_h.erl
+++ src/syslog_logger_h.erl
@@ -46,8 +46,6 @@
 -define(FORMATTER, {logger_formatter,
                     #{single_line => true, template => [msg]}}).
 
--dialyzer({no_missing_calls, log_extra_report/4}).
-
 %%%=============================================================================
 %%% API
 %%%=============================================================================
